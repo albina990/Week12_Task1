bba95e9e6944066e0cc03c8af0fbc8891bd227c9
albina990 <albina.khadieva90@gmail.com>
2023-04-04 15:57:56 +0200

    добавлена функция checkMember

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,62 +1,78 @@
-let member = document.querySelector('input#name');
-let avatar = document.querySelector('input#avatar');
-let chat = document.querySelector('.chat');
-let comment = document.querySelector('textarea#comment');
-let submit = document.querySelector('input#submit');
+let member = document.querySelector("input#name");
+let avatar = document.querySelector("input#avatar");
+let chat = document.querySelector(".chat");
+let comment = document.querySelector("textarea#comment");
+let submit = document.querySelector("input#submit");
 
-submit.addEventListener('click', sendPost);
+submit.addEventListener("click", sendPost);
 
 function checkSpam(str) {
-    let str1 = str.replace(/viagra/i, '***');
-    let str2 = str1.replace(/XXX/i, '***');
+    let str1 = str.replace(/viagra/i, "***");
+    let str2 = str1.replace(/XXX/i, "***");
     return str2;
 }
-function sendPost(event){
-    event.preventDefault();
-    let avatarLink = avatar.value;
-    let memberNameLow = member.value.toLowerCase().trim();
-    let firstSpacePosition = memberNameLow.indexOf(' ');
-    let lastSpacePosition = memberNameLow.lastIndexOf(' ');
-    
+function checkMember(str) {
+    let memberNameLow = str.value.toLowerCase().trim();
+    let firstSpacePosition = memberNameLow.indexOf(" ");
+    let lastSpacePosition = memberNameLow.lastIndexOf(" ");
+
     let memberName;
     let memberSirname;
     let memberLastname;
     let memberSirnameLow;
 
-    if(firstSpacePosition !== -1 && lastSpacePosition !== -1 && lastSpacePosition !== firstSpacePosition){
-        console.log(lastSpacePosition)
-        memberName = memberNameLow[0].toUpperCase() + memberNameLow.slice(1, firstSpacePosition);
-        memberSirnameLow = memberNameLow.slice(firstSpacePosition + 1, lastSpacePosition).trim();
-        memberSirname = memberSirnameLow[0].toUpperCase() + memberSirnameLow.slice(1, lastSpacePosition).trim();
+    if (
+        firstSpacePosition !== -1 &&
+        lastSpacePosition !== -1 &&
+        lastSpacePosition !== firstSpacePosition
+    ) {
+        console.log(lastSpacePosition);
+        memberName =
+            memberNameLow[0].toUpperCase() +
+            memberNameLow.slice(1, firstSpacePosition);
+        memberSirnameLow = memberNameLow
+            .slice(firstSpacePosition + 1, lastSpacePosition)
+            .trim();
+        memberSirname =
+            memberSirnameLow[0].toUpperCase() +
+            memberSirnameLow.slice(1, lastSpacePosition).trim();
         let memberLastnameLow = memberNameLow.slice(lastSpacePosition + 1);
-        memberLastname = memberLastnameLow[0].toUpperCase() + memberLastnameLow.slice(1)
-    } else if((lastSpacePosition == firstSpacePosition) && firstSpacePosition !== -1){
-        memberName = memberNameLow[0].toUpperCase() + memberNameLow.slice(1, firstSpacePosition);
+        memberLastname =
+            memberLastnameLow[0].toUpperCase() + memberLastnameLow.slice(1);
+    } else if (
+        lastSpacePosition == firstSpacePosition &&
+        firstSpacePosition !== -1
+    ) {
+        memberName =
+            memberNameLow[0].toUpperCase() +
+            memberNameLow.slice(1, firstSpacePosition);
         memberSirnameLow = memberNameLow.slice(firstSpacePosition + 1);
-        console.log();
-        memberSirname = memberSirnameLow[0].toUpperCase() + memberSirnameLow.slice(1).trim();
-        memberLastname = '';
+        memberSirname =
+            memberSirnameLow[0].toUpperCase() + memberSirnameLow.slice(1).trim();
+        memberLastname = "";
     } else {
         memberName = memberNameLow[0].toUpperCase() + memberNameLow.slice(1);
-        memberSirname = '';
-        memberLastname = '';
+        memberSirname = "";
+        memberLastname = "";
     }
+    return `${memberName} ${memberSirname} ${memberLastname}`;
+}
+function sendPost(event) {
+    event.preventDefault()
+    let vullName = checkMember(member);
+    let avatarLink = avatar.value;
 
-let commentContent = checkSpam(comment.value);
-    chat.innerHTML = chat.innerHTML + `<div class="chat__item">
+    let commentContent = checkSpam(comment.value);
+    chat.innerHTML =
+        chat.innerHTML +
+        `<div class="chat__item">
 <div class="chat__member">
     <img
         src="${avatarLink}"
         alt="avatar"
     />
-    <p class="name">${memberName} ${memberSirname} ${memberLastname}</p>
+    <p class="name">${vullName}</p>
 </div>
 <p class="comment">${commentContent}</p>
 </div>`;
 }
-
-
-
-
-
-
