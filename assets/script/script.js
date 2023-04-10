6c09db86b3d1cc12d5426bf81f3c96b15ee63192
albina990 <albina.khadieva90@gmail.com>
2023-04-10 22:41:02 +0200

    fix script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,67 +1,23 @@
-let member = document.querySelector("input#name");
-let avatar = document.querySelector("input#avatar");
+let member = document.getElementById("name");
+let avatar = document.getElementById("avatar");
 let chat = document.querySelector(".chat");
-let comment = document.querySelector("textarea#comment");
-let submit = document.querySelector("input#submit");
+let comment = document.getElementById("comment");
+let submit = document.getElementById("submit");
 
 submit.addEventListener("click", sendPost);
 
 function checkSpam(str) {
-    let str1 = str.replace(/viagra/gi, "***");
-    let str2 = str1.replace(/виагра/gi, "***");
-    let str3 = str2.replace(/XXX/gi, "***");
-    return str3;
+    let newStr = str.replace(/viagra/gi, "***").replace(/виагра/gi, "***").replace(/XXX/gi, "***");
+    return newStr;
 }
 function checkMember(str) {
-    let memberNameLow = str.value.toLowerCase().trim();
-    let firstSpacePosition = memberNameLow.indexOf(" ");
-    let lastSpacePosition = memberNameLow.lastIndexOf(" ");
-
-    let memberNameSpace = memberNameLow.slice(firstSpacePosition + 1, lastSpacePosition);
-    let memberName;
-    let memberSirname;
-    let memberLastname;
-    let memberSirnameLow;
-
-    if (
-        firstSpacePosition !== -1 &&
-        lastSpacePosition !== -1 &&
-        lastSpacePosition !== firstSpacePosition &&
-        !memberNameSpace.includes(' ')
-    ) {
-        memberName =
-            memberNameLow[0].toUpperCase() +
-            memberNameLow.slice(1, firstSpacePosition);
-        memberSirnameLow = memberNameLow
-            .slice(firstSpacePosition + 1, lastSpacePosition)
-            .trim();
-        memberSirname =
-            memberSirnameLow[0].toUpperCase() +
-            memberSirnameLow.slice(1, lastSpacePosition).trim();
-        let memberLastnameLow = memberNameLow.slice(lastSpacePosition + 1);
-        memberLastname =
-            memberLastnameLow[0].toUpperCase() + memberLastnameLow.slice(1);
-    } else if (
-        lastSpacePosition == firstSpacePosition &&
-        firstSpacePosition !== -1
-    ) {
-        memberName =
-            memberNameLow[0].toUpperCase() +
-            memberNameLow.slice(1, firstSpacePosition);
-        memberSirnameLow = memberNameLow.slice(firstSpacePosition + 1);
-        memberSirname =
-            memberSirnameLow[0].toUpperCase() + memberSirnameLow.slice(1).trim();
-        memberLastname = "";
-    } else {
-        memberName = memberNameLow[0].toUpperCase() + memberNameLow.slice(1);
-        memberSirname = "";
-        memberLastname = "";
-    }
-    return `${memberName} ${memberSirname} ${memberLastname}`;
+    let fullName = str.value.toLowerCase().trim().split(/\s+/).map(val => val[0].toUpperCase() + val.substring(1)).join(' ');
+    return fullName;
 }
+
 function sendPost(event) {
     event.preventDefault()
-    let vullName = checkMember(member);
+    let fullName = checkMember(member);
     let avatarLink = avatar.value;
 
     let commentContent = checkSpam(comment.value);
@@ -73,7 +29,7 @@ function sendPost(event) {
         src="${avatarLink}"
         alt="avatar"
     />
-    <p class="name">${vullName}</p>
+    <p class="name">${fullName}</p>
 </div>
 <p class="comment">${commentContent}</p>
 </div>`;
